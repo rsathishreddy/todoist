f9718a73d6d3f18738edf2458042e4cff27e4e1e
Sathish Reddy <sathishreddy00@gmail.com>
2020-10-13 16:51:13 +0100

    Updates

---
+++ precache-manifest.2c38bc122a234afd01191d9e842548f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e220c8ba845c1b7ae50c5dbcd9016fe4",
+    "url": "/todoist/index.html"
+  },
+  {
+    "revision": "af9f4e63977d5e9c932b",
+    "url": "/todoist/static/css/2.daa8c32e.chunk.css"
+  },
+  {
+    "revision": "833e95d337f5ed35e4fe",
+    "url": "/todoist/static/css/main.0ea6109a.chunk.css"
+  },
+  {
+    "revision": "af9f4e63977d5e9c932b",
+    "url": "/todoist/static/js/2.16a1ba45.chunk.js"
+  },
+  {
+    "revision": "735c9e4fd3b679ca5e031d54b3cb5e70",
+    "url": "/todoist/static/js/2.16a1ba45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833e95d337f5ed35e4fe",
+    "url": "/todoist/static/js/main.235e3fcb.chunk.js"
+  },
+  {
+    "revision": "3487eb9fa42481bcfd9a",
+    "url": "/todoist/static/js/runtime-main.74e31d91.js"
+  }
+]);
